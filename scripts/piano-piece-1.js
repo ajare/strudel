2c36833259ee90d37cea8154ed2b74e79796851e
Will Marsh <wtmrsh@gmail.com>
2026-02-09 23:14:42 +0000

    intro work and tweaks

--- scripts/piano-piece-1.js
+++ scripts/piano-piece-1.js
@@ -1,25 +1,29 @@
-setcpm(85 / 4)
+setcpm(75 / 4)
 
-// Use euclidean rhythm for much of it, maybe changing the first value keeping the second (8)
-// We can increase the "energy" by increasing the first value
+// Change euclidean params can change energy - change step from 1 up to 5
 
-// Common "tail"
+const p1 = n("<0 4 ~ [2 4]!2 3 ~ 3 6>").euclid("<2 4 5 7>/4", 8)  
+  .scale("D:minor").scaleTrans("<0 7>");
+
+const p2 = n("[0 <5 6> 4 <7*2 2>]").euclid(5, 8)
+  .sometimesBy(0.125, ply("2"))
+  .s("piano")
+  .scale("D:minor").scaleTrans("<0 7 14 <21 12>>");
+
+const p3 = n("<0 4 ~ 5 3 2>").euclid(5, 8)
+  .scale("D:minor").scaleTrans("<0 12>");
+
+$piano: seqPLoop(
+  [0, 16, p1],
+  [16, 24, p2],
+  [24, 32, p3])
   .s("piano")
   .lpf(rand.range(300, 500))
   .lpq(5)
   .delay(rand.range(0.5, 0.8))
   .room(0.5)
   .postgain(0.6)
-  
-// Complex part which can be built up and played with a bit
-$piano: n("[0 <5 6> 4 <7*2 2>]").euclid(5, 8)
-  .sometimesBy(0.125, ply("2"))
-  .scale("D:minor").scaleTrans("<0 7 14 <21 12>>")
-
-// Slow part.  This could be part of an interlude where
-// we rely more or less on the chords
-$piano: n("<0 4 ~ 5 3 2>").euclid(5, 8)
-  .scale("D:minor").scaleTrans("<0 12>")
+  ._pianoroll()
 
 //
 // Basic chords
@@ -31,7 +35,11 @@ const c2 = "[d3, f3, a3]*2 [a3, c4, e4]*2 [f3, a3, c4]*2 <[g4, a#4, d5] [d3, f3,
 // For .slow(4)
 const c4 = "[d3, f3, a3] [a3, c4, e4] [f3, a3, c4] <[g4, a#4, d5] [d3, f3, a3]>"
 
-$chords: note(c4).slow(4)
+$chords: seqPLoop(
+  [0, 16, note(c4).slow(4)],
+  [16, 24, note(c2).slow(2)],
+  [24, 32, note(c4).slow(4)],
+  )
   .s("piano")
   .lpf(500)
   .postgain(0.4)
